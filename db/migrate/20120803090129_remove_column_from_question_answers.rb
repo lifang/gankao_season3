7af7653680f59273cfddedc82b3ba940e732f5ae
xuya <xuyae573@foxmail.com>
2012-08-03 17:07:45 +0800

    modfity migrate

--- db/migrate/20120803090129_remove_column_from_question_answers.rb
+++ db/migrate/20120803090129_remove_column_from_question_answers.rb
@@ -1,6 +1,6 @@
 class RemoveColumnFromQuestionAnswers < ActiveRecord::Migration
   def up
-    remove_column :quer_question_id
+    remove_column :question_answers,:quer_question_id
     add_column :question_answers,:user_question_id,:integer
   end
 
