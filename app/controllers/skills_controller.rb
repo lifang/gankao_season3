f505588779bc4083f7c5f484537aa464b1b7b0d9
qianjun <er6788@126.com>
2012-08-09 12:39:56 +0800

    qianjun

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,16 @@
 class SkillsController < ApplicationController
   layout 'main'
   def index
+    category=params[:category_id].nil? ? 2 : params[:category_id]
+    skills=Skill.find_by_sql("select types,skill_title,skill_url from skills where category_id=#{category} order by created_at")
+    @skills={}
+    skills.each do |skill|
+      if  @skills[skill.types].nil?
+        @skills[skill.types]=[skill]
+      else
+        @skills[skill.types] << skill
+      end
+    end unless skills.blank?
   end
 
 end
