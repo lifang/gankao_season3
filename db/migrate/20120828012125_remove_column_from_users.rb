50f35879681d66952ad76917a384439278e94841
lifang <santhe_2004@126.com>
2012-08-28 11:39:13 +0800

    update detial

--- db/migrate/20120828012125_remove_column_from_users.rb
+++ db/migrate/20120828012125_remove_column_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveColumnFromUsers < ActiveRecord::Migration
   def up
-    remove_column :users,:singin_days
-    add_column :uses,:singin_days,:string , :default=>"CET4=>0,CET6=>0,GRADUATE=>0"
+    remove_column :users,:signin_days
+    add_column :users, :signin_days, :string , :default=>"CET4=>0,CET6=>0,GRADUATE=>0"
   end
 
   def down
