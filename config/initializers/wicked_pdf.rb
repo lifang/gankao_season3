62e35a77f46574d89afd1666c857527e50bd5c88
qianjun <er6788@126.com>
2012-09-28 11:58:35 +0800

    qianjun

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
-#WickedPdf.config = {
+WickedPdf.config = {
   #:exe_path => "/usr/local/bin/wkhtmltopdf"
-#  :exe_path => "D:/Program Files/wkhtmltopdf/wkhtmltopdf.exe"
+  :exe_path => "D:/Program Files/wkhtmltopdf/wkhtmltopdf.exe"
 
-#}
+}
